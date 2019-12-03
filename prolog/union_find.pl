46e6693feeefab1a11780c6d5af1d9c225bf9d6f
José Antonio Riaza Valverde <riazavalverde@gmail.com>
2019-12-03 12:42:12 +0100

    added make_set/2 predicate

--- prolog/union_find.pl
+++ prolog/union_find.pl
@@ -1,6 +1,6 @@
 /**
   * 
-  * FILENAME: union-find.pl
+  * FILENAME: union_find.pl
   * DESCRIPTION: This module contains predicates for manipulating union-find structures.
   * AUTHORS: José Antonio Riaza Valverde <riaza.valverde@gmail.com>
   * GITHUB: https://github.com/jariazavalverde/prolog-union-find
@@ -12,6 +12,7 @@
 
 :- module(union_find, [
     union_find/2,
+    make_set/2,
     union/3,
     find/3
 ]).
@@ -26,6 +27,13 @@ union_find(I, N, [I-0|UF]) :-
     succ(I, J),
     union_find(J, M, UF).
 
+make_set(UF0, UF1) :-
+    UF0 =.. [union_find|Args0],
+    length(Args0, N),
+    succ(N, M),
+    append(Args0, [M-0], Args1),
+    UF1 =.. [union_find|Args1].
+
 union(UF, I, J) :-
     find_with_rank(UF, I, X-RankI),
     find_with_rank(UF, J, Y-RankJ),
@@ -38,8 +46,8 @@ union(UF, I, J) :-
 
 find(UF, I, X) :-
     arg(I, UF, J-R),
-    (I == J -> X = J ; find(UF, J, X), setarg(I, UF, X-R)).
+    (I == J -> X = J ; find(UF, J, X), nb_setarg(I, UF, X-R)).
 
 find_with_rank(UF, I, X-S) :-
     arg(I, UF, J-R),
-    (I == J -> X = J, S = R ; find_with_rank(UF, J, X-S), setarg(I, UF, X-R)).
+    (I == J -> X = J, S = R ; find_with_rank(UF, J, X-S), nb_setarg(I, UF, X-R)).
